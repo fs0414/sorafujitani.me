eb9205c81ff76ee44d8eda6ab56db020870c4736
fujitani sora <fujitanisora0414@gmail.com>
2026-02-12 19:19:48 +0900

    oss button

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,10 @@ export const SITE_URL = 'https://sorafujitani.me';
 export const SITE_NAME = 'Sora Fujitani';
 export const GITHUB_USERNAME = 'fs0414';
 export const PULLS_HIDDEN_REPOS: string[] = [];
+export const PULLS_OWN_REPOS: string[] = [
+  'fs0414/rfmt',
+  'fs0414/tech-event-scheduler',
+  'fs0414/neovimdot',
+  'fs0414/sorafujitani.me',
+];
 export const PULLS_SINCE = '2025-10-04';
